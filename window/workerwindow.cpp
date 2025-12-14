91d019ae443de262cc083e7a64a4b6779f435d40
Taras <luviq7@gmail.com>
2025-12-14 21:27:40 +0200

    added workerwindow .h .cpp .ui

---
+++ window/workerwindow.cpp
@@ -0,0 +1,5 @@
+#include "workerwindow.h"
+
+WorkerWindow::WorkerWindow()
+{
+}
---
+++ window/workerwindow.h
@@ -0,0 +1,10 @@
+#ifndef WORKERWINDOW_H
+#define WORKERWINDOW_H
+
+class WorkerWindow
+{
+public:
+    WorkerWindow();
+};
+
+#endif // WORKERWINDOW_H
