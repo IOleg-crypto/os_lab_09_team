c379eac7cd369a1f0b4da266e0a4430677a99424
Taras <luviq7@gmail.com>
2025-12-14 22:18:38 +0200

    adding manual ideas input streams

--- window/workerwindow.cpp
+++ window/workerwindow.cpp
@@ -1,5 +1,39 @@
 #include "workerwindow.h"
+// ВАЖЛИВО: Оскільки файл називається workerwindow.ui (маленькими),
+// то інклуд має бути саме таким:
+#include "ui_workerwindow.h"
 
-WorkerWindow::WorkerWindow()
+#include <QDateTime>
+
+WorkerWindow::WorkerWindow(int workerId, VirtualBoard* board, QWidget *parent)
+    : QMainWindow(parent),
+      ui(std::make_unique<Ui::WorkerWindow>()),
+      m_board(board),
+      m_workerId(workerId)
 {
+    ui->setupUi(this);
+
+    // Встановимо заголовок
+    setWindowTitle(QString("Worker #%1").arg(workerId));
+
+           // Підключимо кнопку
+    connect(ui->btnSend, &QPushButton::clicked, this, &WorkerWindow::onSendClicked);
+}
+
+WorkerWindow::~WorkerWindow() = default;
+
+void WorkerWindow::onSendClicked()
+{
+    QString text = ui->lineEditIdea->text();
+    if (text.isEmpty()) return;
+
+    if (m_board) {
+        // ТУТ БУЛА ПОМИЛКА з типами.
+        // Я поміняв аргументи місцями (текст, потім ID).
+        // Якщо знову буде помилка - поміняй назад: (m_workerId, text.toStdString())
+        m_board->SubmitIdea(text.toStdString(), m_workerId);
+    }
+
+    ui->listMyIdeas->addItem(text);
+    ui->lineEditIdea->clear();
 }
--- window/workerwindow.h
+++ window/workerwindow.h
@@ -1,10 +1,38 @@
 #ifndef WORKERWINDOW_H
 #define WORKERWINDOW_H
 
-class WorkerWindow
+#include <QMainWindow>
+#include <memory>
+// Додаємо ці інклуди, щоб Qt знав типи об'єктів
+#include <QLineEdit>
+#include <QPushButton>
+#include <QListWidget>
+#include <QLabel>
+#include <QVBoxLayout>
+
+// Твій клас для зв'язку (перевір шлях, якщо він в іншій папці)
+#include "../ipc/VirtualBoard.h"
+
+namespace Ui {
+class WorkerWindow;
+}
+
+class WorkerWindow : public QMainWindow
 {
+    Q_OBJECT
+
 public:
-    WorkerWindow();
+    // Конструктор приймає ID воркера і вказівник на дошку
+    explicit WorkerWindow(int workerId, VirtualBoard* board, QWidget *parent = nullptr);
+    ~WorkerWindow();
+
+private slots:
+    void onSendClicked(); // Слот для натискання кнопки
+
+private:
+    std::unique_ptr<Ui::WorkerWindow> ui;
+    VirtualBoard* m_board;
+    int m_workerId;
 };
 
 #endif // WORKERWINDOW_H
